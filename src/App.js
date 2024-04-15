19ff86c872ceed5d663d28772b2815a69824dfb9
Damitht <tenna_koon@yahoo.com>
2024-04-15 15:52:49 +1000

    React - animals project - Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function useState(defaultValue){
+    return {
+        yourState: defaultValue,
+        yourSetter: () => {}
+    };
+}
+
+function App(){
+
+//////////////////////////////////////////////
+    // function makeArray() {
+    //     return [1, 10, 32, 40];
+    // }
+
+    // const myArray = makeArray();
+    // const firstElement = myArray[0];
+    // const secondElement = myArray[2];
+    // console.log(firstElement, secondElement);
+
+    // //Array destructuring
+    // const [firstEle, secondEle] = makeArray();
+    // console.log(firstElement, secondElement);
+
+ ////////////////////////////////////////////////   
+    //const handleMouseMove = () => {
+    //    console.log('Button was clicked!');
+    //}
+
+    //const handleClick = () => {
+    //    console.log('Button was clicked!');
+    //}
+
+/////////////////////////////////////////////////
+
+
+const{ yourSetter} = useState(0);
+
+
+/////////////////////////////////////////////////
+    //const [count, setCount] = useState(0);
+    // const handleClick = () => {
+    //     setCount(count + 1);
+    // };
+
+    // return (
+    //     <div>
+    //         <button onClick={handleClick}>Add Animal Click</button>
+    //         <div>Number of Animals : {count}</div>
+    //        {/*<button onMouseMove={handleMouseMove}>Add Animal Move</button>*/}
+    //     </div>
+    // );
 }
 
 export default App;
